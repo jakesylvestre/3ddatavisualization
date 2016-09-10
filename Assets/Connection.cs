856f5a3e80afd9c113d7b5394df64e9cdd97dfca
Jake Sylvestre <jakesyl@gmail.com>
2016-09-09 21:26:39 -0400

    add note class

---
+++ Assets/Connection.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Connection{
+	private double weight;
+	private string key;
+}
---
+++ Assets/Node.cs
@@ -0,0 +1,3 @@
+﻿public class Node{
+	private string key;
+}
